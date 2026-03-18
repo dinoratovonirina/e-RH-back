9bc4b4e218e975891358ded89bc789b489e7bf87
dinoratovonirina <ratovonirinadino@gmail.com>
2026-03-18 11:49:43 +0100

    first commit

---
+++ src/main/java/com/example/e_RH/ERhApplication.java
@@ -0,0 +1,13 @@
+package com.example.e_RH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/e_RH/ERhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_RH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
